9043b05b3de4412a2174fc790d8d134a37fa5a98
fengwj <gorillaz.f@gmail.com>
2015-05-12 10:43:44 +0800

    modify demo html
    1.add random transition to demo html;
    2.move custom page view from page.js to demo html

--- src/page.js
+++ src/page.js
@@ -1,17 +1,12 @@
 //所有page的基类
 //如果一个属性只在pad下面有意义，命名时遵循 xxxxIfPad 的约定
 Eip.Pages.PageView = Backbone.View.extend({
-	tagName: 'div',
-	pageId: '',						 //page的Id
-	transitionType: '',				 //入场动画类型， '' - 默认的滑动，none - 没有动画
-	isDraggable: true,				  // 这个page是不是能够随着手指拖动？
-	pageType: '',					   //页面类型, homepage表示这个是homepage
+	tagName: 	'div',
+	pageId: 	'',		//page的Id
+	pageType: 	'',		//页面类型, homepage表示这个是homepage
 	pageDragger: null,
-
-	//以下属性只在pad下有意义
-	isAdaptableIfPad: false,			//在pad下是否做出适应性的变化？
-	isFirstPageIfPad: false,			//这个page是不是该卡片的第一个page？
-	isDraggableIfPad: true,
+	isDraggable: true,	//这个page是不是能够随着手指拖动？
+	transitionType: '',	//入场动画类型， '' - 默认的滑动，none - 没有动画
 
 	initialize: function(options) {
 		var self = this;
@@ -36,107 +31,4 @@ Eip.Pages.PageView = Backbone.View.extend({
 	//动画结束后
 	onTransitionEnd: function() {}
 
-});
-
-
-Eip.Pages.RedPageView = Eip.Pages.PageView.extend({
-	init: function(){
-		this.$el.css({
-			'background-color':'red',
-			'display':'block',
-			'height':'100%',
-			'width':'100%'
-		});
-
-		var prevBtn =
-		$("<div class='prevBtn' \
-				style='float: left;\
-						display: block;\
-						margin-top: 100px;\
-						width: 50px;\
-						height: 30px;'>\
-		</div>");
-		prevBtn.text('PREV');
-
-		var nextBtn =
-		$("<div class='nextBtn' \
-				style='float: right;\
-						display: block;\
-						margin-top: 100px;\
-						width: 50px;\
-						height: 30px;'>\
-		</div>");
-		nextBtn.text('NEXT');
-
-		this.$el.append(prevBtn);
-		this.$el.append(nextBtn);
-	}
-});
-
-Eip.Pages.BluePageView = Eip.Pages.PageView.extend({
-	init: function(){
-		this.$el.css({
-			'background-color':'blue',
-			'display':'block',
-			'height':'100%',
-			'width':'100%'
-		});
-
-		var prevBtn =
-		$("<div class='prevBtn' \
-				style='float: left;\
-						display: block;\
-						margin-top: 100px;\
-						width: 50px;\
-						height: 30px;'>\
-		</div>");
-		prevBtn.text('PREV');
-
-		var nextBtn =
-		$("<div class='nextBtn' \
-				style='float: right;\
-						display: block;\
-						margin-top: 100px;\
-						width: 50px;\
-						height: 30px;'>\
-		</div>");
-		nextBtn.text('NEXT');
-
-		this.$el.append(prevBtn);
-		this.$el.append(nextBtn);
-	}
-});
-
-Eip.Pages.GreenPageView = Eip.Pages.PageView.extend({
-	init: function(){
-		this.$el.css({
-			'background-color':'green',
-			'display':'block',
-			'height':'100%',
-			'width':'100%'
-		});
-
-		var prevBtn =
-		$("<div class='prevBtn' \
-				style='float: left;\
-						display: block;\
-						margin-top: 100px;\
-						width: 50px;\
-						height: 30px;'>\
-		</div>");
-		prevBtn.text('PREV');
-
-		var nextBtn =
-		$("<div class='nextBtn' \
-				style='float: right;\
-						display: block;\
-						margin-top: 100px;\
-						width: 50px;\
-						height: 30px;'>\
-		</div>");
-		nextBtn.text('NEXT');
-
-		this.$el.append(prevBtn);
-		this.$el.append(nextBtn);
-	}
 });
