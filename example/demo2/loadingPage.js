54495b35666585f771e6483ea9cee919fec07332
fengwj <gorillaz.f@gmail.com>
2015-05-16 17:22:30 +0800

    improve loading page for demo2
    improve message after resources loaded, show delay count.

--- example/demo2/loadingPage.js
+++ example/demo2/loadingPage.js
@@ -6,12 +6,18 @@ nut.Pages.LoadingPageView = nut.Pages.PageView.extend({
         this.$el.addClass('loading-page');
         var text = $('<div class="loading-text"></div>');
         text.append('<span>感谢你关注我，请往下看吧</span>');
-        text.append('<p>这次启动加载，合计耗时：'+this.options.cost+'ms</p>');
+
+        var memo = $('<div class="memo-text"></div>');
+        memo.append('<p>network cost：'+this.options.cost+'ms</p>');
         if (Number(this.options.delay)>100) {
-            text.append('<p>但加载的动画做得这么好看</p>');
-            text.append('<p>我就让你多看了一会：'+this.options.delay+'ms</p>');
+            memo.append('<p>force wait：'+this.options.delay+'ms</p>');
+            memo.append('<p>total wait：'+ (Number(this.options.delay)+Number(this.options.cost)) +'ms</p>');
+        }
+        else {
+            memo.append('<p>total wait：'+this.options.delay+'ms</p>');
         }
         this.$el.append(text);
+        this.$el.append(memo);
 		this.$el.append('<div class="arrow-down"></div>');
     },
     doNext: function(){
